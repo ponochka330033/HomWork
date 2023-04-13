644bd164be1b2ee7ec6de7c7de2f02c1b08deadf
Helen <ponochka330033@yandex.ru>
2023-04-13 21:51:09 +0300

    Задача 8

---
+++ Example8/Program.cs
@@ -0,0 +1,21 @@
+﻿int i = 1;
+bool not = true;
+
+Console.WriteLine("Введите число: ");
+string userInput = Console.ReadLine() ??"";
+int Num = int.Parse(userInput);
+
+Console.WriteLine("Четные числа от 1 до " + Num);
+while (i <= Num)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ",");
+        not = false;
+    }
+    i++;
+}
+if (not)
+{
+    Console.WriteLine("Нет четных чисел!");
+}
