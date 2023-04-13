2b009f5a06143803a2eed3be1b5604dd3d91b0e2
Helen <ponochka330033@yandex.ru>
2023-04-13 20:55:51 +0300

    Задача 4

---
+++ Example4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+Console.WriteLine("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+Console.WriteLine("Введите третье число: ");
+string userInput3 = Console.ReadLine() ?? "";
+int userNum1 = int.Parse (userInput1);
+int userNum2 = int.Parse (userInput2);
+int userNum3 = int.Parse (userInput3);
+int max = userNum1;
+if (userNum2 > max)
+{ 
+    max = userNum2;
+}
+if (userNum3 > max)
+{
+    max = userNum3;
+}
+Console.WriteLine($"{userNum1} {userNum2} {userNum3} -> {max}");
