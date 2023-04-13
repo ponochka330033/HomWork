e909192fcc822ecc76a3e73108a772a8187ec6b8
Helen <ponochka330033@yandex.ru>
2023-04-13 21:34:47 +0300

    Задача 6

---
+++ Example6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число");
+string userInput = Console.ReadLine() ?? "";
+
+int userNum = int.Parse(userInput);
+if (userNum %2 == 0)
+{
+    Console.WriteLine($"{userNum} -> да");
+}
+else
+{
+    Console.WriteLine($"{userNum} -> нет");
+}
