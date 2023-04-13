bbdef3b9d7f0f63c11c0e9385e5d536688e8833f
Helen <ponochka330033@yandex.ru>
2023-04-13 20:12:22 +0300

    Задача 2

---
+++ Example2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+Console.WriteLine("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int a = int.Parse (userInput1);
+int b = int.Parse (userInput2);
+
+if (a > b)
+{
+    Console.WriteLine ("{a} -> max");
+}
+else
+{
+    Console.WriteLine ("{b} -> max");
+}
