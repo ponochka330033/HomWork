c13728beae0ca3cbf8f4bc53ae2feb979163a6e1
Helen <ponochka330033@yandex.ru>
2023-04-20 14:53:52 +0300

    Задача 23

---
+++ Example23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int N = int.Parse (userInput);
+
+ int i = 1;
+while (i <= N)
+{
+    Console.WriteLine ($"{N} -> {Math.Pow(i, 3)}");
+     i++;
+}
