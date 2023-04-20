1de10cffcd5707e3a007fe588e257ea5adad7d7d
Helen <ponochka330033@yandex.ru>
2023-04-20 15:50:02 +0300

    Задача 19

---
+++ Example19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write ("Введите число: ");
+string palindrome = Console.ReadLine () ?? "";
+int count = palindrome.Length;
+
+if (count == 5)
+{
+    if (palindrome[0] == palindrome[4] && palindrome[1] == palindrome[3])
+    {
+        Console.WriteLine ("Палиндром");
+    }
+    else
+    {
+        Console.WriteLine ("Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine ("Ошибка ввода");
+}
