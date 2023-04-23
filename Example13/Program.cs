e8d4cc5fc1fdfb82b63c0373d530e22cfc2c661d
Helen <ponochka330033@yandex.ru>
2023-04-23 13:16:15 +0300

    Задача 13

---
+++ Example13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int Number = int.Parse (userInput);
+int Num = 0;
+
+if (Number >= 100)
+{
+    while (Number > 999)
+    {
+        Number = Number / 10;
+    }
+    Num = Number % 10;
+    Console.WriteLine($"{Number} -> {Num}");
+}
+else 
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
