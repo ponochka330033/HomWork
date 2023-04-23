0b6635fa6ece798cc331a7129950d135f66f522c
Helen <ponochka330033@yandex.ru>
2023-04-23 12:39:27 +0300

    Задача 10

---
+++ Example10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трехзначное число: ");
+string userInput = Console.ReadLine() ?? "";
+int Num = int.Parse(userInput);
+int a = Num/10;
+int b = a % 10;
+Console.WriteLine ($"Вторая цифра числа {Num} -> {b}");
+
