a6ea6e8b9a2856aea539a3272a0a91a9b71e0230
Helen <ponochka330033@yandex.ru>
2023-04-23 13:33:10 +0300

    Задача 15

---
+++ Example15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите номер дня недели: ");
+string userInput = Console.ReadLine () ?? "";
+int a = int.Parse (userInput);
+
+if (a > 1 && a < 5)
+{
+    Console.WriteLine ("не является выходным");
+}
+
+else if (a == 6 || a == 7)
+{
+    Console.WriteLine ("Является выходным");
+}
+else
+{
+    Console.WriteLine ("Нет такого дня недели");
+}
