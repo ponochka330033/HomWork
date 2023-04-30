892ba5a61c5a46bf4bb58299fd35060a105b50bd
Helen <ponochka330033@yandex.ru>
2023-04-30 14:36:20 +0300

    Задача 25

---
+++ Example25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход
+// два числа А и В и возводит число А в натуральную
+// степень В.
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+
+if(b<1)
+{
+    Console.Write("Число B должно быть натуральным\n");
+}
+else
+{
+    while (b!=0)
+    {
+        c = c * a;
+        b = b - 1;
+    }
+    Console.Write($"Число А в натуральной степени В равно {c}");
+}
