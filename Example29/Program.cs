bcb0ab555466e7afdddcdecb888fe4cd192728a7
Helen <ponochka330033@yandex.ru>
2023-04-30 16:03:04 +0300

    Задача 29

---
+++ Example29/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
+1,2,5,7,,19,6,1,33 -> [1,2,5,7,19,6,1,33] */
+
+int[] arr = { 1, 2, 5, 7, 19, 6, 1, 33 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
